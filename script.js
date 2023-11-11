77788c4d26077a063480d7e288be9d73e6ed8a16
hekin43 <karima.yamaleeva@mail.ru>
2023-11-12 02:08:04 +0300

    feat: add first button animation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const expertBtn = document.querySelector(".buttonExp");
+const popUpWindow = document.querySelector(".popUpWindow");
+const cross = document.querySelector(".union");
 
+expertBtn.addEventListener("click", function() {
+	popUpWindow.classList.toggle('hidden');
+})
+
+cross.addEventListener("click", function() {
+	popUpWindow.classList.add('hidden');
+})
